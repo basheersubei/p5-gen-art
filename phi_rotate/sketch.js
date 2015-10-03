04f0309120f504074798e7baebc535bc801f97c3
Basheer Subei <basheersubei@gmail.com>
2015-10-03 18:21:06 -0400

    tweaks values to match phi logo and pauses on mouseClick.

--- phi_rotate/sketch.js
+++ phi_rotate/sketch.js
@@ -10,15 +10,16 @@ var theta;
 var n;
 var separation_factor;
 var size_factor;
+var is_rotating;
 
 function setup() {
   noStroke();
   createCanvas(displayWidth, displayHeight);
-  theta = 2 * PI * (sqrt(5.0)-1) / 2.0;  // golden ratio angle
-  n = 100;                               // number of circles
-  separation_factor = 1.2;  // 1.2 is default
-  size_factor = 0.4;  // 0.4 is default
-
+  theta = -2 * PI * (sqrt(5.0)-1) / 2.0;  // golden ratio angle
+  n = 83;                               // number of circles
+  separation_factor = 0.11; // 1.2 is default
+  size_factor = 1.2;  // 0.4 is default
+  is_rotating = true;
   frameRate(30);
 
   angleMode(RADIANS);
@@ -30,12 +31,14 @@ function draw() {
   for (var i = 0; i < n; i++) {
     //Trippy swirling effect:
     var sq = sqrt(i/separation_factor) * 10;
-    var mouse_x = map(mouseX, 0, width, 1, 100);
-    var mouse_y = map(mouseY, 0, height, 0, 3.14);
-    var radius = 0.04 * (5 + pow(i, size_factor)) * sin(i/mouse_x + mouse_y)*20;
+    // var mouse_x = map(mouseX, 0, width, 1, 40);
+    // var mouse_y = map(mouseY, 0, height, 0, 3.14);
+    var mouse_x = 35;
+    var mouse_y = 0.8; 
+    var radius = 0.04 * (130+ pow(i, size_factor)) * sin(i/mouse_x + mouse_y)*4.5;
     if (frameCount % 10 == 0) {
-      console.log("mouse_x: " + mouse_x);
-      console.log("mouse_y: " + mouse_y);
+      // console.log("mouse_x: " + mouse_x);
+      // console.log("mouse_y: " + mouse_y);
     }
     //Original effect:
     //var radius = 0.04 * (5 + pow(i, size_factor)) * mouseY / 5;
@@ -43,10 +46,23 @@ function draw() {
    
     push();
     translate(width/2, height/2);
-    rotate(theta*frameCount);
-    ellipse(sq * sin(theta * i), sq * cos(theta * i), radius, radius);
+    scale(0.3);
+    // TODO fix bug when frameCount gets too high, rotate becomes inaccurate
+    if (is_rotating) {
+      angle = theta*frameCount;
+    } else {
+      angle = theta;
+    }
+    //rotate(-angle);
+    rotate(angle);
+    if (i > 1) {
+    	ellipse(sq * sin(theta * i), sq * cos(theta * i), radius, radius);
+    }
     pop();
   }
 
 }
 
+function mouseClicked() {
+  is_rotating = !is_rotating;
+}
