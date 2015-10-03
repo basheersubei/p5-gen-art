5504abc9b2dc302ae9773fdbc697b418e5715c6b
Hazem <halabihazem@gmail.com>
2015-10-03 18:35:39 +0300

    adds swirl effect

--- phi_rotate/sketch.js
+++ phi_rotate/sketch.js
@@ -12,7 +12,8 @@ var separation_factor;
 var size_factor;
 
 function setup() {
-  createCanvas(640,640);
+  noStroke();
+  createCanvas(displayWidth, displayHeight);
   theta = 2 * PI * (sqrt(5.0)-1) / 2.0;  // golden ratio angle
   n = 500;                               // number of circles
   separation_factor = 1.2;  // 1.2 is default
@@ -24,10 +25,17 @@ function setup() {
 }
 
 function draw() {
-  background(51);
+  background(color(113, 87, 84));
+  fill(color(214,115,113));
   for (var i = 0; i < n; i++) {
-    var sq = sqrt(i/separation_factor) * mouseX / 40;
-    var radius = 0.04 * (5 + pow(i, size_factor)) * mouseY / 5;
+    //Trippy swirling effect:
+    var sq = sqrt(i/separation_factor) * 18;
+    var radius = 0.04 * (5 + pow(i, size_factor)) * sin(i/10)*30;
+    
+    //Original effect:
+    //var radius = 0.04 * (5 + pow(i, size_factor)) * mouseY / 5;
+    //var sq = sqrt(i/separation_factor) * mouseX / 40;
+   
     push();
     translate(width/2, height/2);
     rotate(theta*frameCount);
