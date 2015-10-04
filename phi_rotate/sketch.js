283bfcf07429f0270890ef6555ac61731a8774a8
Basheer Subei <basheersubei@gmail.com>
2015-10-03 19:24:52 -0500

    fixes shaking bug when count gets too high for float precision

--- phi_rotate/sketch.js
+++ phi_rotate/sketch.js
@@ -11,11 +11,12 @@ var n;
 var separation_factor;
 var size_factor;
 var is_rotating;
-
 function setup() {
   noStroke();
   createCanvas(displayWidth, displayHeight);
-  theta = -2 * PI * (sqrt(5.0)-1) / 2.0;  // golden ratio angle
+  theta = new Big(-2 * PI * (sqrt(5.0)-1) / 2.0);  // golden ratio angle
+  angle = new Big(theta);
+
   n = 83;                               // number of circles
   separation_factor = 0.11; // 1.2 is default
   size_factor = 1.2;  // 0.4 is default
@@ -29,17 +30,14 @@ function draw() {
   background(color(113, 87, 84));
   fill(color(214,115,113));
   for (var i = 0; i < n; i++) {
-    //Trippy swirling effect:
+    // Trippy swirling effect:
     var sq = sqrt(i/separation_factor) * 10;
     // var mouse_x = map(mouseX, 0, width, 1, 40);
     // var mouse_y = map(mouseY, 0, height, 0, 3.14);
     var mouse_x = 35;
     var mouse_y = 0.8; 
     var radius = 0.04 * (130+ pow(i, size_factor)) * sin(i/mouse_x + mouse_y)*4.5;
-    if (frameCount % 10 == 0) {
-      // console.log("mouse_x: " + mouse_x);
-      // console.log("mouse_y: " + mouse_y);
-    }
+
     //Original effect:
     //var radius = 0.04 * (5 + pow(i, size_factor)) * mouseY / 5;
     //var sq = sqrt(i/separation_factor) * mouseX / 40;
@@ -47,13 +45,13 @@ function draw() {
     push();
     translate(width/2, height/2);
     scale(0.3);
-    // TODO fix bug when frameCount gets too high, rotate becomes inaccurate
+
     if (is_rotating) {
-      angle = theta*frameCount;
+      angle = theta.times(frameCount % 9000);
     } else {
       angle = theta;
     }
-    //rotate(-angle);
+    // rotate(-angle);  // rotates inwards
     rotate(angle);
     if (i > 1) {
     	ellipse(sq * sin(theta * i), sq * cos(theta * i), radius, radius);
