534aa91062e0c525f9fb77d82fd5ce62ffe0c190
Hazem <halabihazem@gmail.com>
2015-10-07 20:27:30 +0300

    Adds color variables.

--- phi_rotate/sketch.js
+++ phi_rotate/sketch.js
@@ -11,24 +11,32 @@ var n;
 var separation_factor;
 var size_factor;
 var is_rotating;
+var logoScaleFactor;
+
+//Color variables
+var bgColor, logoColor;
+
+
 function setup() {
   noStroke();
   createCanvas(displayWidth, displayHeight);
   theta = new Big(-2 * PI * (sqrt(5.0)-1) / 2.0);  // golden ratio angle
   angle = new Big(theta);
-
   n = 83;                               // number of circles
   separation_factor = 0.11; // 1.2 is default
   size_factor = 1.2;  // 0.4 is default
   is_rotating = true;
   frameRate(30);
-
   angleMode(RADIANS);
+  logoScaleFactor = 0.2;
+  //Color variables
+  bgColor = color(43,10,24);
+  logoColor = color (255,80);
 }
 
 function draw() {
-  background(color(113, 87, 84));
-  fill(color(214,115,113));
+  background(bgColor);
+  fill(logoColor);
   for (var i = 0; i < n; i++) {
     // Trippy swirling effect:
     var sq = sqrt(i/separation_factor) * 10;
@@ -44,7 +52,7 @@ function draw() {
    
     push();
     translate(width/2, height/2);
-    scale(0.3);
+    scale(logoScaleFactor);
 
     if (is_rotating) {
       angle = theta.times(frameCount % 9000);
